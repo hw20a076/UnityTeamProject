fc04db6b897befb85da6874750062eb5e8725146
Ryusei Kawamoto <hw20a035@oecu.jp>
2023-10-13 13:59:11 +0900

    動くスクリプトをオブジェクトに追加

---
+++ Assets/run.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class run : MonoBehaviour
+{
+    public float speed = 2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void FixedUpdate()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Rigidbody rigidbody = GetComponent<Rigidbody>();
+
+        rigidbody.AddForce(x * speed, 0, z * speed);
+    }
+
+}
